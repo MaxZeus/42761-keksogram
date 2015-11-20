d932fc828f7e3219e0991bf9eabf70be479eb4cb
Сергей Попов <tovarov.piter@gmail.com>
2015-11-21 00:16:50 +0300

    Добавил размер картинки над картинкой

--- js/resizer.js
+++ js/resizer.js
@@ -130,6 +130,18 @@
           this._resizeConstraint.side - this._ctx.lineWidth / 2,
           this._resizeConstraint.side - this._ctx.lineWidth / 2);
 
+      // работа с текстом размера картинки
+      var imageW = this._image.naturalWidth;
+      var imageH = this._image.naturalHeight;
+      var text = imageW + " x " + imageH;
+      var textCtx = this._ctx.measureText(text);
+      var textW = textCtx.width;
+
+      // Цвет заливки.
+      this._ctx.fillStyle = 'rgb(255,255,255)';
+      this._ctx.font = "14px normal Arial sans-serif";
+      this._ctx.fillText(text, (this._ctx.lineWidth / 2 - textW/2 ), ((-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2 - 10));
+
       // Восстановление состояния канваса, которое было до вызова ctx.save
       // и последующего изменения системы координат. Нужно для того, чтобы
       // следующий кадр рисовался с привычной системой координат, где точка
